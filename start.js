2732f7533b379d02608c86025e4c7ba03368da35
Tingan Ho <tingan87@gmail.com>
2014-03-21 16:31:35 +0800

    Fixed typo

--- start.js
+++ start.js
@@ -3,7 +3,6 @@ var express = require('express')
   , http = require('http')
   , https = require('https')
   , fs = require('fs')
-  , os = require('os')
   , privateKey  = fs.readFileSync('sslcert/server.key', 'utf8')
   , certificate = fs.readFileSync('sslcert/server.crt', 'utf8')
   , credentials = { key: privateKey, cert: certificate };
@@ -16,7 +15,7 @@ var certificate = fs.readFileSync('sslcert/server.crt');
 
 app.use(express.static(__dirname));
 
-app.use(express.bodyParser({ uploadDir: os.tmpdir() }));
+app.use(express.bodyParser({ uploadDir: __dirname + '/tmp' }));
 app.use(express.logger('dev'));
 app.use(express.query());
 app.use(express.compress());
